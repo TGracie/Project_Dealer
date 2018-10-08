2f86df6eca5d19db41c7a4ec4adef5f9b1f294c7
Thomas Gracie <tgracie030@gmail.com>
2018-10-08 10:46:24 +0100

    Added car index and working on new car page, trying to figure out how to link to specific shop ids here, may just shortcut for now

--- controllers/cars_controller.rb
+++ controllers/cars_controller.rb
@@ -1,7 +1,22 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
-require_relative( './models/car.rb' )
-require_relative( './models/customer.rb' )
-require_relative( './models/shop.rb' )
-also_reload( './models/*' )
+require_relative( '../models/car.rb' )
+require_relative( '../models/customer.rb' )
+require_relative( '../models/shop.rb' )
+also_reload( '../models/*' )
+
+get '/cars' do
+  @cars = Car.all
+  erb( :"cars/index" )
+end
+
+get '/cars/new' do
+  erb(:"cars/new")
+end
+
+post '/cars' do
+  car = Car.new(params)
+  car.save
+  redirect to("/cars")
+end
--- controllers/customers_controllers.rb
+++
@@ -1,7 +0,0 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require( 'pry-byebug' )
-require_relative( './models/car.rb' )
-require_relative( './models/customer.rb' )
-require_relative( './models/shop.rb' )
-also_reload( './models/*' )
